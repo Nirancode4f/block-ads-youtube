8214bb73392225c7412d67c0d3046ee3e969d45b
Nirancode4f <78076796+Nirancode4f@users.noreply.github.com>
2021-10-28 14:15:44 +0700

    Update content.js

--- block-ads for youtube/content.js
+++ block-ads for youtube/content.js
@@ -1,11 +1,11 @@
 const container = document.getElementsByClassName("video-ads ytp-ad-module")[0];
 
 const observer = new MutationObserver(() => {
-   // Lấy element button
+  
   
    const skipButton = document.getElementsByClassName("ytp-ad-skip-button ytp-button")[0];
    if (skipButton){
-      // Nhấn vào button để skip quảng cáo
+
       skipButton.click();     
    }
    
